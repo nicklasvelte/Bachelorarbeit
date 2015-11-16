7a757016c3224001a6fc071eb29bfe1fafd5e488
nicklasvelte <nicklas.velte@hotmail.de>
2015-11-16 18:07:48 +0100

    originaler MapReduce Code

--- mrRed.java
+++ mrRed.java
@@ -22,7 +22,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStream; 
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
